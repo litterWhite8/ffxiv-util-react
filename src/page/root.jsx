e582185c70fd6dd2787c11ed44816b30b4b5973c
litterWhite <3433138054@qq.com>
2024-01-28 16:57:21 +0800

    overlay.js和悬浮窗监听的实现

--- src/page/root.jsx
+++ src/page/root.jsx
@@ -1,43 +1,19 @@
+import {useEffect} from "react";
+import "../util/overlay.js"
+
 export default function Root() {
+
+    useEffect(()=> {
+        addOverlayListener("LogLine",data => {
+            console.log(data)
+            console.log(1)
+        })
+        startOverlayEvents()
+    })
+
     return (
-        <>
-            <div id="sidebar">
-                <h1>React Router Contacts</h1>
-                <div>
-                    <form id="search-form" role="search">
-                        <input
-                            id="q"
-                            aria-label="Search contacts"
-                            placeholder="Search"
-                            type="search"
-                            name="q"
-                        />
-                        <div
-                            id="search-spinner"
-                            aria-hidden
-                            hidden={true}
-                        />
-                        <div
-                            className="sr-only"
-                            aria-live="polite"
-                        ></div>
-                    </form>
-                    <form method="post">
-                        <button type="submit">New</button>
-                    </form>
-                </div>
-                <nav>
-                    <ul>
-                        <li>
-                            <a href={`/contacts/1`}>Your Name</a>
-                        </li>
-                        <li>
-                            <a href={`/contacts/2`}>Your Friend</a>
-                        </li>
-                    </ul>
-                </nav>
-            </div>
-            <div id="detail"></div>
-        </>
+       <div>
+           1234
+       </div>
     );
 }
