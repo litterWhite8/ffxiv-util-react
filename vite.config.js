1cfc695f3c386c964a6f48162661598e3d1f929a
litterWhite <3433138054@qq.com>
2024-01-28 23:53:03 +0800

    修改配置文件

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000,
+    // proxy: {
+    //   '/api': {
+    //     target: 'http://your-api-server.com',
+    //     changeOrigin: true,
+    //   },
+    // },
+    open: true,
+    historyApiFallback: true,
+  },
 })
